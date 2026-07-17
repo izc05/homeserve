98a25982941604fe9c3effafae68f429b7b9c025
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-07-17 18:28:43 +0200

    feat: añadir entrada del prototipo demo

---
+++ src/demo-main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DemoApp from './DemoApp';
+import './styles/global.css';
+import './styles/auth.css';
+import './styles/real-data.css';
+import './styles/demo-mode.css';
+import './styles/demo-shell.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <DemoApp />
+  </React.StrictMode>,
+);
