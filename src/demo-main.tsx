e995b4000bdf3d5383bebe8dbbbcb139f3179c81
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-07-18 17:11:25 +0200

    Cargar estilos de equipos demo

--- src/demo-main.tsx
+++ src/demo-main.tsx
@@ -11,6 +11,7 @@ import './styles/demo-execution.css';
 import './styles/ui-polish.css';
 import './styles/demo-planning.css';
 import './styles/demo-technician.css';
+import './styles/demo-assets.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
