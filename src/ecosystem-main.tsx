606432e9ae4e49001758b11de1266c17eb397bb9
IsiVoltPro Mantenimiento <isiciozafracantos@gmail.com>
2026-08-04 10:10:08 +0200

    feat(ecosystem): bootstrap ecosystem application

---
+++ src/ecosystem-main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EcosystemApp from './EcosystemApp';
+import './styles/ecosystem.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <EcosystemApp />
+  </React.StrictMode>,
+);
